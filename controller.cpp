e9caff5f58f981c48378c0a6aab39033feb32b58
Tuan Anh <162298246+TuanAnh20102003@users.noreply.github.com>
2025-03-07 10:05:07 +0700

    Update controller.cpp

--- controller.cpp
+++ controller.cpp
@@ -1,11 +1,12 @@
 
 #include "controller.h"
-
+#include "modbus_helper.h"
 
 #define pi 3.141592653589793
 double travel_in_one_rev = 0.655; // meters
 int cpr = 16385; // counts per revolution
 double R_Wheel = 0.105; // meters
+
     Controller::Controller(const std::string &port) {
         // Initialize Modbus context
         ctx = modbus_new_rtu(port.c_str(), 115200, 'N', 8, 1);
@@ -27,6 +28,7 @@ double R_Wheel = 0.105; // meters
     Controller::~Controller() {
         modbus_close(ctx);
         modbus_free(ctx);
+		std::cout << "Controller destructor called." << std::endl;
     }
     
     double rpm_to_radPerSec(double rpm) 
@@ -38,7 +40,8 @@ double R_Wheel = 0.105; // meters
         double W_Wheel = rpm_to_radPerSec(rpm);
         return W_Wheel * R_Wheel;
     }
-    void Controller::set_mode(int mode) {
+    //chon mode (1,2,3) 3: speed mode, 2: position mode, 1:
+    void Controller::set_mode(const uint16_t mode) {
         if (mode < 1 || mode > 3) {
             std::cerr << "set_mode ERROR: set only 1, 2, or 3" << std::endl;
             return;
@@ -46,7 +49,7 @@ double R_Wheel = 0.105; // meters
         if(mode == 1) std::cout << "POS_REL_CONTROL" << std::endl;
         else if(mode == 2) std::cout << "POS_ABS_CONTROL" << std::endl;
         else if(mode == 3) std::cout << "VEL_CONTROL" << std::endl;
-        modbus_write_register(ctx, OPR_MODE, mode);
+        write_register(ctx, OPR_MODE, mode);
     }
 
     //not fixed
@@ -54,7 +57,7 @@ double R_Wheel = 0.105; // meters
     uint16_t mode_register;
     
     // Read 1 holding register starting from OPR_MODE
-    int rc = modbus_read_registers(ctx, OPR_MODE, 1, &mode_register);
+    int rc = read_register(ctx, OPR_MODE, 1, &mode_register);
     
     // Error handling
     if (rc == -1) {
@@ -68,32 +71,32 @@ double R_Wheel = 0.105; // meters
 
 
     void Controller::enable_motor() {
-        modbus_write_register(ctx, CONTROL_REG, ENABLE);
+        write_register(ctx, CONTROL_REG, ENABLE);
     }
 
     void Controller::disable_motor() {
-        modbus_write_register(ctx, CONTROL_REG, DOWN_TIME);
+        write_register(ctx, CONTROL_REG, DOWN_TIME);
     }
 
     std::pair<bool, uint16_t> Controller::get_fault_code() {
         uint16_t fault_codes[2];
-        modbus_read_registers(ctx, L_FAULT, 2 , fault_codes);
+        read_register(ctx, L_FAULT, 2 , fault_codes);
         bool L_fault_flag = std::find(FAULT_LIST.begin(), FAULT_LIST.end(), fault_codes[0]) != FAULT_LIST.end();
         bool R_fault_flag = std::find(FAULT_LIST.begin(), FAULT_LIST.end(), fault_codes[1]) != FAULT_LIST.end();
         return {L_fault_flag, R_fault_flag};
     }
 
     void Controller::clear_alarm() {
-        modbus_write_register(ctx, CONTROL_REG, ALRM_CLR);
+        write_register(ctx, CONTROL_REG, ALRM_CLR);
     }
-
+    
     void Controller::set_accel_time(int L_ms, int R_ms) {
         if(L_ms >32767) L_ms = 32767;
         else if(L_ms < 0) L_ms = 0;
         if(R_ms >32767) R_ms = 32767;
         else if(R_ms < 0) R_ms = 0;
         uint16_t values[2] = {static_cast<uint16_t>(L_ms), static_cast<uint16_t>(R_ms)};
-        modbus_write_registers(ctx, L_ACL_TIME, 2, values);
+        write_register(ctx, L_ACL_TIME, 2, values);
     }
 
     void Controller::set_decel_time(int L_ms, int R_ms) {
@@ -102,7 +105,7 @@ double R_Wheel = 0.105; // meters
         if(R_ms >32767) R_ms = 32767;
         else if(R_ms < 0) R_ms = 0;
         uint16_t values[2] = {static_cast<uint16_t>(L_ms), static_cast<uint16_t>(R_ms)};
-        modbus_write_registers(ctx, L_DCL_TIME, 2, values);
+        write_register(ctx, L_DCL_TIME, 2, values);
     }
 
     uint16_t int16_to_uint16(int value) 
@@ -118,12 +121,12 @@ double R_Wheel = 0.105; // meters
         uint16_t right_bytes = int16_to_uint16(R_rpm);
         
         uint16_t values[2] = {left_bytes, right_bytes};
-        modbus_write_registers(ctx, L_CMD_RPM, 2, values);
+        write_register(ctx, L_CMD_RPM, 2, values);
     }
 
     std::pair<double, double> Controller::get_rpm() {
         uint16_t registers[2];
-        modbus_read_registers(ctx, L_FB_RPM, 2, registers);
+        read_register(ctx, L_FB_RPM, 2, registers);
         double fb_L_rpm = static_cast<int16_t>(registers[0]) / 10.0;
         double fb_R_rpm = static_cast<int16_t>(registers[1]) / 10.0;
         return {fb_L_rpm, fb_R_rpm};
@@ -143,7 +146,7 @@ double R_Wheel = 0.105; // meters
         if(max_R_rpm > 1000) max_R_rpm = 1000;
         else if(max_R_rpm < 1) max_R_rpm = 1;
         uint16_t values[2] = {static_cast<uint16_t>(max_L_rpm), static_cast<uint16_t>(max_R_rpm)};
-        modbus_write_registers(ctx, L_MAX_RPM_POS, 2, values);
+        write_register(ctx, L_MAX_RPM_POS, 2, values);
     }
     
      int32_t map(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max) {
@@ -166,17 +169,19 @@ double R_Wheel = 0.105; // meters
         std::vector<uint16_t> all_cmds_array;
         all_cmds_array.insert(all_cmds_array.end(), L_array.begin(), L_array.end());
         all_cmds_array.insert(all_cmds_array.end(), R_array.begin(), R_array.end());
-        modbus_write_registers(ctx, L_CMD_REL_POS_HI, all_cmds_array.size(), all_cmds_array.data());
+        write_register(ctx, L_CMD_REL_POS_HI, all_cmds_array.size(), all_cmds_array.data());
     }
 
     std::pair<double, double> Controller::get_wheels_travelled() {
         uint16_t registers[4];
-        modbus_read_registers(ctx, L_FB_POS_HI, 4, registers);
+        read_register(ctx, L_FB_POS_HI, 4, registers);
         int32_t l_pulse = ((registers[0] & 0xFFFF) << 16) | (registers[1] & 0xFFFF);
         int32_t r_pulse = ((registers[2] & 0xFFFF) << 16) | (registers[3] & 0xFFFF);
         double L_meters = travel_in_one_rev * l_pulse / cpr;
         double R_meters = travel_in_one_rev * r_pulse / cpr;
         return {L_meters, R_meters};
     }
-
+	void Controller::stop() {
+		write_register(ctx, CONTROL_REG, EMER_STOP);
+	}
 
